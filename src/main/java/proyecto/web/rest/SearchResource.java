6a685c36471bd2bf1c390c0ab6aaa34701bd7466
Alan <alanvallve@gmail.com>
2017-02-19 11:05:38 +0100

    #1 Empezado filtro de usuarios

--- src/main/java/proyecto/web/rest/SearchResource.java
+++ src/main/java/proyecto/web/rest/SearchResource.java
@@ -11,7 +11,9 @@ import proyecto.repository.PhotoRepository;
 import proyecto.repository.UserRepository;
 
 import java.net.URISyntaxException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Alan on 17/02/2017.
@@ -28,13 +30,27 @@ public class SearchResource {
 
     private OfferRepository offerRepository;
 
-//    @PostMapping(value = "/search/users", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-//    @Timed
-//    public List<User> searchUsers(@RequestParam String algo) throws URISyntaxException {
-//
-//
-//
-//        return null;
-//    }
+    @PostMapping(value = "/search/users", produces = MediaType.APPLICATION_JSON_VALUE)
+    @Timed
+    public List<User> searchUsers(
+        @RequestParam(value = "userName", required = false) String userName,
+        @RequestParam(value = "City", required = false) String city,
+        @RequestParam(value = "points", required = false) Double points,
+        @RequestParam(value = "tags", required = false) String tags,
+        @RequestParam(value = "validated", required = false) boolean validated,
+        @RequestParam(value = "ageMin", required = false) int ageMin,
+        @RequestParam(value = "ageMax", required = false) int ageMax
+    ) throws URISyntaxException {
+
+        Map<String, Object> params = new HashMap<>();
+
+        if (userName != null) {
+            params.put("username", userName);
+        }
+        if(city != null){
+
+        }
+        return null;
+    }
 
 }
