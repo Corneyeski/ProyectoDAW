70a2a8de945b8a5b04b4a1ddad53aa5c5e01dcdf
corneyeski <alanvallve@gmail.com>
2017-02-24 16:26:32 +0100

    #1 Filtro funcionando con un atributo

--- src/main/java/proyecto/repository/UserExtCriteriaRepository.java
+++ src/main/java/proyecto/repository/UserExtCriteriaRepository.java
@@ -2,6 +2,8 @@ package proyecto.repository;
 
 import org.hibernate.Criteria;
 import org.hibernate.Session;
+import org.hibernate.criterion.MatchMode;
+import org.hibernate.criterion.Restrictions;
 import org.springframework.stereotype.Repository;
 import proyecto.domain.UserExt;
 
@@ -20,25 +22,63 @@ public class UserExtCriteriaRepository {
         return entityManager.unwrap(Session.class);
     }
 
-    /*public List<UserExt> filterUserextDefinitions(Map<String, Object> parameters) {
+    public List<UserExt> filterUserextDefinitions(Map<String, Object> parameters) {
 
         Criteria userExtDefinitionCriteria = currentSession().createCriteria(UserExt.class);
         userExtDefinitionCriteria.setResultTransformer(Criteria.DISTINCT_ROOT_ENTITY);
 
-        Criteria mottoCriteria = userExtDefinitionCriteria.createCriteria("userext");
+        //Criteria mottoCriteria = userExtDefinitionCriteria.createCriteria("userext");
 
-        filterBySearchBy(parameters, mottoCriteria);
+        filterByUserExt(parameters, userExtDefinitionCriteria);
 
-        filterByCategoria(parameters, userExtDefinitionCriteria);
+        /*filterByCategoria(parameters, userExtDefinitionCriteria);
 
         filterByMateria(parameters, userExtDefinitionCriteria);
 
         filterByRegion(parameters, userExtDefinitionCriteria);
 
-        filterByRegistro(parameters, userExtDefinitionCriteria);
+        filterByRegistro(parameters, userExtDefinitionCriteria);*/
 
-        List<UserExt> results = mottoCriteria.list();
+        List<UserExt> results = userExtDefinitionCriteria.list();
 
         return results;
-    }*/
+    }
+
+    private void filterByUserExt(Map<String, Object> parameters, Criteria userExtCriteria) {
+
+
+        String searchName = (String) parameters.get("city");
+        userExtCriteria.add(Restrictions.ilike("city", searchName, MatchMode.ANYWHERE));
+
+        //TODO: Aquí es dónde se filtra el tipo de búsqueda ( Empieza por, acaba en, etc...) habría que añadir u case más para el default o poner en el front a default el 1 y que quite el restriction
+
+    }
+
+    private void filterByCategoria(Map<String, Object> parameters, Criteria mottoDefinitionCriteria) {
+        if (parameters.containsKey("categorias")) {
+            String[] categorias = (String[]) parameters.get("categorias");
+            mottoDefinitionCriteria.add(Restrictions.in("categoria", categorias));
+        }
+    }
+
+    private void filterByMateria(Map<String, Object> parameters, Criteria mottoDefinitionCriteria) {
+        if (parameters.containsKey("materias")) {
+            String[] materias = (String[]) parameters.get("materias");
+            mottoDefinitionCriteria.add(Restrictions.in("materia", materias));
+        }
+    }
+
+    private void filterByRegion(Map<String, Object> parameters, Criteria mottoDefinitionCriteria) {
+        if (parameters.containsKey("regiones")) {
+            String[] regiones = (String[]) parameters.get("regiones");
+            mottoDefinitionCriteria.add(Restrictions.in("region", regiones));
+        }
+    }
+
+    private void filterByRegistro(Map<String, Object> parameters, Criteria mottoDefinitionCriteria) {
+        if (parameters.containsKey("registros")) {
+            String[] registros = (String[]) parameters.get("registros");
+            mottoDefinitionCriteria.add(Restrictions.in("registro", registros));
+        }
+    }
 }
--- src/main/java/proyecto/web/rest/SearchResource.java
+++ src/main/java/proyecto/web/rest/SearchResource.java
@@ -5,11 +5,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import proyecto.domain.User;
 import proyecto.domain.UserExt;
 import proyecto.repository.*;
 
+import javax.inject.Inject;
+import javax.transaction.Transactional;
 import java.net.URISyntaxException;
 import java.util.HashMap;
 import java.util.List;
@@ -23,37 +26,41 @@ public class SearchResource {
 
     private UserRepository userRepository;
 
-    private UserExtCriteriaRepository userExtCriteriaRepository;
+    @Inject
+    UserExtCriteriaRepository userExtCriteriaRepository;
 
     private PhotoRepository photoRepository;
 
     private OfferRepository offerRepository;
 
-    @PostMapping(value = "/search/users", produces = MediaType.APPLICATION_JSON_VALUE)
+    @RequestMapping(value = "/search/users",
+        method = RequestMethod.GET,
+        produces = MediaType.APPLICATION_JSON_VALUE)
     @Timed
-    public List<User> searchUsers(
-        @RequestParam(value = "userName", required = false) String userName,
-        @RequestParam(value = "City", required = false) String city,
+    @Transactional
+    public ResponseEntity<List<UserExt>> searchUsers(
+        @RequestParam(value = "city", required = false) String city
+        /*@RequestParam(value = "City", required = false) String city
         @RequestParam(value = "points", required = false) Double points,
         @RequestParam(value = "tags", required = false) String tags,
         @RequestParam(value = "validated", required = false) boolean validated,
         @RequestParam(value = "ageMin", required = false) int ageMin,
-        @RequestParam(value = "ageMax", required = false) int ageMax
+        @RequestParam(value = "ageMax", required = false) int ageMax*/
     ) throws URISyntaxException {
 
         Map<String, Object> params = new HashMap<>();
 
-        if (userName != null) {
-            params.put("username", userName);
+        if (city != null) {
+            params.put("city",city);
         }
-        if(city != null){
+        /*if(city != null){
             params.put("city",city);
         }
         if(points != null && points > 0){
             params.put("points",points);
         }
         if(tags != null){
-
+            //TODO bucle para sacar cada tag
         }
         if(validated){
             params.put("validated",validated);
@@ -63,14 +70,13 @@ public class SearchResource {
         }
         if(ageMax > 0){
             params.put("agemax",ageMax);
-        }
+        }*/
 
-        /*List<UserExt> result = userExtCriteriaRepository.filterUserextDefinitions(params);
+        List<UserExt> result = userExtCriteriaRepository.filterUserextDefinitions(params);
 
         return new ResponseEntity<>(
             result,
-            HttpStatus.OK);*/
-        return null;
+            HttpStatus.OK);
     }
 
 }
