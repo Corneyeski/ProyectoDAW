97051f73e86d778433ec3186a1708bfd412c8c20
46989075Y <ricardhuelamo@gmail.com>
2017-02-24 16:56:12 +0100

    BUG FIXED Redirección botón register de la plantilla custom (navbar.html). Añadida separación main.css.

--- src/main/webapp/app/account/register/register.state.js
+++ src/main/webapp/app/account/register/register.state.js
@@ -8,27 +8,7 @@
     stateConfig.$inject = ['$stateProvider'];
 
     function stateConfig($stateProvider) {
-        $stateProvider.state('register', {
-            parent: 'account',
-            url: '/register',
-            data: {
-                authorities: [],
-                pageTitle: 'register.title'
-            },
-            views: {
-                'content@': {
-                    templateUrl: 'app/account/register/register.html',
-                    controller: 'RegisterController',
-                    controllerAs: 'vm'
-                }
-            },
-            resolve: {
-                translatePartialLoader: ['$translate', '$translatePartialLoader', function ($translate, $translatePartialLoader) {
-                    $translatePartialLoader.addPart('register');
-                    return $translate.refresh();
-                }]
-            }
-        });
+
 
 
         $stateProvider.state('register-2', {
@@ -40,6 +20,27 @@
             views: {
                 'content@': {
                     templateUrl: 'app/account/register/register2.html',
+                    controller: 'RegisterController',
+                    controllerAs: 'vm'
+                }
+            },
+            resolve: {
+                translatePartialLoader: ['$translate', '$translatePartialLoader', function ($translate, $translatePartialLoader) {
+                    $translatePartialLoader.addPart('register');
+                    return $translate.refresh();
+                }]
+            }
+        });
+        $stateProvider.state('register', {
+            parent: 'account',
+            url: '/register',
+            data: {
+                authorities: [],
+                pageTitle: 'register.title'
+            },
+            views: {
+                'content@': {
+                    templateUrl: 'app/account/register/register.html',
                     controller: 'RegisterController',
                     controllerAs: 'vm'
                 }
