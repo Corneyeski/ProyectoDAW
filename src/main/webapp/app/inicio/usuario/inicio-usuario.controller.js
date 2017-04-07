9cc983525a11a6a683e91a80679da2c5ecc81adf
46989075Y <ricardhuelamo@gmail.com>
2017-04-07 18:26:12 +0200

    #7 Imprime todas las imagenes que pasa el Back. Falta implementar nginfinite scroll.

--- src/main/webapp/app/inicio/usuario/inicio-usuario.controller.js
+++ src/main/webapp/app/inicio/usuario/inicio-usuario.controller.js
@@ -10,8 +10,21 @@
 
     function InicioController($translate, $timeout, Auth, LoginService, UserExt, entity) {
         var vm = this;
+        var array2=[];
+
 
         vm.photosDTO = entity[0];
+
+        console.log(array2);
+        console.log(entity);
+
+
+        vm.loadMore = function() {
+            for(var i=0;i<entity.length;i++){
+                array2.push(entity[i]);
+            }
+            vm.photos=array2;
+        };
 
 
     }
