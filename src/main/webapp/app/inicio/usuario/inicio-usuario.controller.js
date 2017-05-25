b127ab8544e0ff73848d3cab2631e91ec5e17b9e
46989075Y <ricardhuelamo@gmail.com>
2017-05-25 21:02:23 +0200

    #7 Paginación

--- src/main/webapp/app/inicio/usuario/inicio-usuario.controller.js
+++ src/main/webapp/app/inicio/usuario/inicio-usuario.controller.js
@@ -5,21 +5,83 @@
         .module('proyectoApp')
         .controller('InicioController', InicioController);
 
-    InicioController.$inject = ['$translate', '$timeout', 'Auth', 'LoginService', 'UserExt', 'entity', 'DataUtils','$uibModal', '$rootScope'];
+    InicioController.$inject = ['$translate', '$timeout', 'Auth', 'LoginService', 'UserExt', 'entity', 'DataUtils','$uibModal', '$rootScope','ParseLinks','paginationConstants','AlertService'];
 
-    function InicioController($translate, $timeout, Auth, LoginService, UserExt, entity, DataUtils,$uibModal, $rootScope) {
+    function InicioController($translate, $timeout, Auth, LoginService, UserExt, entity, DataUtils,$uibModal, $rootScope,ParseLinks,paginationConstants,AlertService) {
         var vm = this;
 
         vm.openModal = openModal;
         vm.closeModal = closeModal;
         vm.enterImg=enterImg;
         vm.leaveImg=leaveImg;
-        initController();
+      //  initController();
 
-        function initController() {
-            vm.bodyText = 'This text can be updated in modal 1';
+        vm.scrolls = [];
+        vm.loadPage = loadPage;
+        vm.itemsPerPage = paginationConstants.itemsPerPage;
+        vm.page = 0;
+        vm.links = {
+            last: 0
+        };
+        vm.predicate = 'id';
+        vm.reset = reset;
+        vm.reverse = true;
+console.log(entity);
+        loadAll();
+
+
+        //
+        // function initController() {
+        //     vm.bodyText = 'This text can be updated in modal 1';
+        // }
+
+        function loadAll () {
+            UserExt.home({
+                page: vm.page,
+                size: vm.itemsPerPage,
+                sort: sort()
+            }, onSuccess, onError);
+            function sort() {
+                var result = [vm.predicate + ',' + (vm.reverse ? 'asc' : 'desc')];
+                if (vm.predicate !== 'id') {
+                    result.push('id');
+                }
+                return result;
+            }
+
+            function onSuccess(data, headers) {
+                vm.totalItems = headers('X-Total-Count');
+               // vm.links = ParseLinks.parse(headers('link'));
+
+
+                console.log(vm.totalItems);
+
+                // for (var i = 0; i < data.length; i++) {
+                //     vm.scrolls.push(data[i]);
+                // }
+
+                for (var i = 0; i < entity.length; i++) {
+                        vm.scrolls.push(entity[i]);
+
+
+                }
+            }
+
+            function onError(error) {
+                AlertService.error(error.data.message);
+            }
         }
 
+        function reset () {
+            vm.page = 0;
+            vm.scrolls = [];
+            loadAll();
+        }
+
+        function loadPage(page) {
+            vm.page = page;
+            loadAll();
+        }
 function enterImg(data){
 
 
@@ -27,7 +89,7 @@ function enterImg(data){
 
 
     data.target.style.transition=".7s ease";
-    console.log(entity[0]);
+
     data.currentTarget.children.info.style.display="inline-block";
 
 }
@@ -47,7 +109,7 @@ function leaveImg(data){
             // var modalInstance
             $rootScope.modalInstance = $uibModal.open({
             template: '<img style="width: auto;height: auto;left:50%;top: 50%;" data-ng-src="'+'data:'+data.photo.imageContentType+';base64,'+data.photo.image+'"</img> ' +
-                    '<button ng-click="vm.closeModal()" type="submit" ui-sref="user-ext-detail({id:'+data.photo.user.id+'})">jooola</button> '
+                    '<button ng-click="vm.closeModal()" type="submit" ui-sref="user-ext-detail({id:'+data.photo.user.id+'})">Perfil Usuario</button> '
 
 
             });
@@ -55,30 +117,31 @@ function leaveImg(data){
         }
 
         function closeModal(){
-            console.log("CERRAR");
+
             //$uibModal.close('my-custom-modal');
             $rootScope.modalInstance.close('mal');
         }
 
 
-        var listImage = [entity[0], entity[1], entity[2], entity[3], entity[4], entity[5], entity[6], entity[7]];
-        var aux = 8;
-        vm.photos2 = listImage;
-        console.log(entity);
-        vm.loadMore = function () {
-            var last = listImage[listImage.length - 1];
-            var j = 8;
-            for (var i = 0; i < 4; i++) {
-                if (aux < entity.length) {
-                    listImage.push(entity[j]);
-                    j++;
-                    aux++;
-                } else {
-                    console.log("no hay mas imagenes");
-                    break;
-                }
-            }
-        };
+        // var listImage = [entity[0], entity[1], entity[2], entity[3], entity[4], entity[5]];
+        // var aux = 6;
+        // vm.photos2 = listImage;
+        //
+        // vm.loadMore = function () {
+        //     var last = listImage[listImage.length - 1];
+        //     var j = 6;
+        //     for (var i = 0; i < 4; i++) {
+        //         if (aux < entity.length) {
+        //             listImage.push(entity[j]);
+        //             j++;
+        //             aux++;
+        //         } else {
+        //
+        //
+        //             break;
+        //         }
+        //     }
+        // };
 
 
 
